d85f137e6266e6b70ae34094882d2865204bf3f3
OyakXD <kayovieira157@gmail.com>
2024-12-10 15:16:43 -0300

    feat: att

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,23 +1,68 @@
 #include "Vector.h"
 
-Vector list;
+Vector::Vector()
+{
+  size = 0;
+  capacity = 5;
+  ptr = new int[capacity];
+}
+
+void Vector::reserve(int n)
+{
+   if(n > capacity)
+    {
+      int *new_ptr = new int[n];
+      for(int i = 0; i < size; i++)
+      {
+        new_ptr[i] = ptr[i];
+      }
+      delete[] ptr;
+      ptr = new_ptr;
+      capacity = n;
+    }
+}
+
+int& Vector::at(int n) const
+{
+  if(n < 0 || n >= size){
+      throw std::out_of_range("Index out of range");
+    }
+    return ptr[n];
+}
+
+void Vector::push_back(int& value){
+  
+  if(size >= capacity){
+    reserve(capacity * 2);
+  }
+      ptr[size] = value;
+      size++;
+}
+
+void Vector::pop_back()
+{
+     if(size > 0)
+    {
+      size--;
+    }
+}
 
 // Troca o elemento no índice k pelo elemento value
-void replaceAt(int value, int k)
+void Vector::replaceAt(int value, int k)
 {
-  if(k >= 0 && k <= list.getSize() - 1){
-    list[k] = value;
+  if(k >= 0 && k <= size - 1){
+    ptr[k] = value;
   }
 }
 
 // Remove o elemento com índice k na lista
-void removeAt(int k)
+void Vector::removeAt(int k)
 {
-  if(k >= 0 && k <= list.getSize() - 1){
-    for(int i = k; i < list.getSize() - 1; i++){
-      list[i] = list[i + 1];
+  if(k >= 0 && k <= size - 1){
+    for(int i = k; i < size - 1; i++){
+      ptr[i] = ptr[i + 1];
     }
-    list.decrementSize();
+    decrementSize();
   }
 }
  /*
@@ -27,26 +72,40 @@ void removeAt(int k)
   são deslocados uma posição para a direita
  
  */
- bool insert(int value, int k)
+ bool Vector::insert(int value, int k)
  {
-  if(k >= 0 && k <= list.getSize() - 1){
-    list.reserve(list.getSize() + 1);
-    for(int i = list.getSize(); i > k; i--){
-      list[i] = list[i - 1];
+  if(k >= 0 && k <= size - 1){
+    reserve(size + 1);
+    for(int i = size; i > k; i--){
+      ptr[i] = ptr[i - 1];
     }
-    list[k] = value;
-    list.incrementSize();
+    ptr[k] = value;
+    incrementSize();
     return true;
   }
  }
 
  // Remove todas as ocorrências do elemento value na lista
- void removeAll(int value)
+ void Vector::removeAll(int value)
  {
-  for(int i = 0; i < list.getSize(); i++){
-    if(list[i] == value){
+  for(int i = 0; i < size; i++){
+    if(ptr[i] == value){
       removeAt(i);
       i--;
     }
   }
+ }
+
+ void Vector::decrementSize()
+ {
+   if(size > 0){
+      --size;
+    }
+ }
+
+ void Vector::incrementSize()
+ {
+  if(size < capacity){
+      ++size;
+    }
  }
